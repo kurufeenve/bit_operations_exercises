8e3e523ae06540f23151e3b0ecdb104751ee4e89
kurufeenve <kurufeenve@gmail.com>
2019-11-24 16:23:37 +0200

    2 more exercises

---
+++ clear_nth_bit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "ft_print_bits.c"
+
+int	main(void)
+{
+	int	number;
+	int	bit;
+	int	mask;
+
+	printf("enter a number: ");
+	scanf("%d", &number);
+	printf("enter a bit position (1-32): ");	
+	scanf("%d", &bit);
+	printf("number before clearing a bit\n");
+	print_bits(number);
+	mask = ~(1 << (bit - 1));
+	number = number & mask;
+	printf("number after clearing a bit\n");
+	print_bits(number);
+	return (0);
+}
--- get_nth_bit.c
+++ get_nth_bit.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+void	print_bits(int	n)
+{
+	char	*bits_array;
+	int	i;
+
+	bits_array = (char *)malloc(sizeof(char) * 33);
+	bits_array[32] = 0;
+	for (i = 31; i >= 0; --i)
+	{
+		bits_array[i] = n & 1;
+		n = n >> 1;
+	}
+	for (i = 0; i < 32; ++i)
+	{
+		printf("%d", bits_array[i]);
+	}
+	printf("\n");
+}
+
 int	main(void)
 {
 	int	number;
